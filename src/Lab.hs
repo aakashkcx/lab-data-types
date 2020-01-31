ff7d1a58b34bbe393a193397528da97b3167d676
Michael Gale <m.gale@warwick.ac.uk>
2020-01-31 15:52:02 +0000

    Add solutions

--- src/Lab.hs
+++ src/Lab.hs
@@ -8,40 +8,45 @@ module Lab where
 --------------------------------------------------------------------------------
 
 data IntPos = IntPos Int Int 
+    -- deriving (Eq, Show)
 
 instance Eq IntPos where 
-    (IntPos x0 y0) == (IntPos x1 y1) = undefined 
+    (IntPos x0 y0) == (IntPos x1 y1) = x0==x1 && y0 == y1 
 
+-- show (IntPos 4 5) => "IntPos 4 5"
 instance Show IntPos where 
-    show (IntPos x y) = undefined 
+    show (IntPos x y) = 
+        "IntPos " ++ show x ++ " " ++ show y
 
 zeroPos :: IntPos 
-zeroPos = undefined 
+zeroPos = IntPos 0 0 
 
 x :: IntPos -> Int 
-x = undefined 
+x (IntPos v _) = v 
 
 y :: IntPos -> Int 
-y = undefined 
+y (IntPos _ v) = v 
 
 --------------------------------------------------------------------------------
 
-data Pos a = Pos a a 
+data Pos a = Pos { left :: a, top :: a } 
+    -- deriving (Eq, Show)
 
-instance Eq a => Eq (Pos a) where 
-    (Pos x0 y0) == (Pos x1 y1) = undefined 
+instance Eq a => Eq (Pos a) where
+    (Pos x0 y0) == (Pos x1 y1) = x0==x1 && y0==y1
 
 instance Show a => Show (Pos a) where 
-    show (Pos x y) = undefined
+    show (Pos x y) = "Pos " ++ show x ++ " " ++ show y
+--     show (Pos x y) = "{" ++ show x ++ "," ++ show y ++ "}"
 
 zero :: Num a => Pos a 
-zero = undefined 
+zero = Pos 0 0 
 
-left :: Pos a -> a 
-left = undefined 
+-- left :: Pos a -> a 
+-- left (Pos v _) = v 
 
-top :: Pos a -> a 
-top = undefined 
+-- top :: Pos a -> a 
+-- top (Pos _ v) = v 
 
 --------------------------------------------------------------------------------
 
@@ -54,7 +59,17 @@ doc =
     , ListItem (\n -> concat ["There. Are. ", show n, ". Items." ])
     ]
 
+--    render [] 1 
+-- => ""
+
+--    render (tail doc) 1 
+-- => "I am item #1\nThere. Are. 2. Items.\n"
+
+--    render doc 1 
+-- => "1. An item\nI am item #2\nThere. Are. 3. Items.\n"
+
 render :: [DocumentItem] -> Int -> String 
-render = undefined
+render [] _ = ""
+render (ListItem f:xs) n = f n ++ "\n" ++ render xs (n+1)
 
 --------------------------------------------------------------------------------
