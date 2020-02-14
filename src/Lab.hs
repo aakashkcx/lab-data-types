f932a5f48282f77679da48dec698b7f396c00399
Aakash Kc <aakash.kc@warwick.ac.uk>
2020-02-14 11:52:35 +0000

    complete

--- src/Lab.hs
+++ src/Lab.hs
@@ -10,38 +10,38 @@ module Lab where
 data IntPos = IntPos Int Int 
 
 instance Eq IntPos where 
-    (IntPos x0 y0) == (IntPos x1 y1) = undefined 
+    (IntPos x0 y0) == (IntPos x1 y1) = x0 == x1 && y0 == y1 
 
 instance Show IntPos where 
-    show (IntPos x y) = undefined 
+    show (IntPos x y) = "IntPos " ++ show x ++ " " ++ show y
 
 zeroPos :: IntPos 
-zeroPos = undefined 
+zeroPos = IntPos 0 0 
 
 x :: IntPos -> Int 
-x = undefined 
+x (IntPos x _) = x
 
 y :: IntPos -> Int 
-y = undefined 
+y (IntPos _ y) = y
 
 --------------------------------------------------------------------------------
 
-data Pos a = Pos a a 
+data Pos a = Pos { left :: a, top :: a}
 
 instance Eq a => Eq (Pos a) where 
-    (Pos x0 y0) == (Pos x1 y1) = undefined 
+    (Pos x0 y0) == (Pos x1 y1) = x0 == x1 && y0 == y1 
 
 instance Show a => Show (Pos a) where 
-    show (Pos x y) = undefined
+    show (Pos x y) = "Pos " ++ show x ++ " " ++ show y
 
 zero :: Num a => Pos a 
-zero = undefined 
+zero = Pos 0 0 
 
-left :: Pos a -> a 
-left = undefined 
+-- left :: Pos a -> a 
+-- left (Pos x _) = x
 
-top :: Pos a -> a 
-top = undefined 
+-- top :: Pos a -> a 
+-- top (Pos _ y) = y
 
 --------------------------------------------------------------------------------
 
@@ -55,6 +55,7 @@ doc =
     ]
 
 render :: [DocumentItem] -> Int -> String 
-render = undefined
+render []     _ = ""
+render ((ListItem x):xs) n = x n ++ "\n" ++ render xs (n+1)
 
 --------------------------------------------------------------------------------
